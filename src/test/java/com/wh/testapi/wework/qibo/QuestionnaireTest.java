047e3b40810c8fb83130aa1ae081c7118ab7562b
wangheng <applewangheng@163.com>
2021-03-01 15:27:02 +0800

    增加测试报告

--- src/test/java/com/wh/testapi/wework/qibo/QuestionnaireTest.java
+++ src/test/java/com/wh/testapi/wework/qibo/QuestionnaireTest.java
@@ -1,5 +1,7 @@
 package com.wh.testapi.wework.qibo;
 
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +10,8 @@ class QuestionnaireTest {
     Questionnaire questionnaire=new Questionnaire();
 
     @Test
+    @Step("我的是step")
+    @Disabled("我是Disb")
     void adminInfo() {
         questionnaire.adminInfo("","");
 
